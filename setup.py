ece945ca13f52652d652c65c0f785c0eca7e38ca
Jaeyup Kim <meloncafe@gmail.com>
2024-07-13 12:34:35 +0900

    modify publish workflow version bump 0.1.1

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="vargur_sdk",
-    version="0.1.0",
+    name="vargur-sdk",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "fastapi",
