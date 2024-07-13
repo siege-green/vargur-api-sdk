84ff24418d062628dce1ecdde05340a6699c68c1
Jaeyup Kim <meloncafe@gmail.com>
2024-07-13 12:47:02 +0900

    add docs version bump 0.1.2

--- setup.py
+++ setup.py
@@ -1,12 +1,27 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="vargur-sdk",
-    version="0.1.1",
+    version="0.1.2",
+    author="MelonCAfe",
+    author_email="contact@siege-green.com",
+    description="A powerful SDK for building extensible applications with plugin support",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meloncafe/vargur-sdk",
     packages=find_packages(),
     install_requires=[
         "fastapi",
+        "uvicorn",
         "sqlalchemy",
+        "aioredis",
+        "pyjwt",
+        "discord.py",
     ],
     extras_require={
         "dev": [
@@ -14,4 +29,13 @@ setup(
             "pytest-asyncio",
         ],
     },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires='>=3.11',
 )
